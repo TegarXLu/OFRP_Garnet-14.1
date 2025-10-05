61d73c17a31c195ec4c237e1e3cb3b8e04fff77f
Tegar Aditia <teg...@gmail.com>
2025-10-05 21:42:03 +0700

    Import isi ZIP ke repo via SSH

---
+++ recovery/root/system/bin/runatboot.sh
@@ -0,0 +1,35 @@
+#!/system/bin/sh
+
+# This script is needed to automatically set props for unified devices.
+
+load_global()
+{
+    echo "POCO F5" > /config/usb_gadget/g1/strings/0x409/product;
+    resetprop "ro.product.brand" "POCO";
+    resetprop "ro.orangefox.device_model" "POCO F5";
+    echo "I:unified-script: setting POCO F5 props" >> $LOGF;
+}
+
+load_CN()
+{
+    echo "Redmi Note 12 Turbo" > /config/usb_gadget/g1/strings/0x409/product;
+    resetprop "ro.product.brand" "Redmi";
+    resetprop "ro.orangefox.device_model" "Redmi Note 12 Turbo";
+    echo "I:unified-script: setting Redmi Note 12 Turbo props" >> $LOGF;
+}
+
+local LOGF=/tmp/recovery.log;
+region=`getprop ro.boot.hwc`;
+
+echo "I:unified-script: detected region:" $region >> $LOGF;
+
+case $region in
+    "CN") # China
+        load_CN
+        ;;
+     *) # Global
+     	load_global
+        ;;
+esac
+
+exit 0
